91d6d5cef8d008ff01724b3bfe96fea926807a1e
Evan Hadfield and Lindsey Baker <lindsey@>
2019-04-02 11:45:00 -0700

    Change forEach to for statement

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,13 @@
 
 $(function(){
   var flavors = ["unicorn", "tree bark", "ear wax"];
+console.log("flavors")
+var total =0
+var icecream = [];
 
-  flavors.forEach(function(flavor){
-    $(".flavors").append("<li>" + flavor + "</li>").val();
-
-  });
+for(var i = 0; i < flavors.length; i ++){
+  total += flavors[i];
+    icecream.unshift("<li>" + flavors[i] + "!!!!!!!" + "</li>");
+  }
+$(".flavors").append(icecream).val();
 });
